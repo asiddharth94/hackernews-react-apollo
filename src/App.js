18ac7a7cac2265fc95f8a4e5e59c3b2bb44d663b
asiddharth94 <49415269+asiddharth94@users.noreply.github.com>
2022-03-25 20:53:55 +0530

    Added Search comp and filtering list based on search

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import LinkList from "./components/LinkList";
 import CreateLink from "./components/CreateLink";
 import Login from "./components/Login";
+import Search from "./components/Search";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<LinkList />} />
           <Route path="/create" element={<CreateLink />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/search" element={<Search />} />
         </Routes>
       </div>
     </div>
